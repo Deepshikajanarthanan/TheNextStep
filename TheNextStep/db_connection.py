3f3c3fbe93b1f2990305da839673689da76d3953
Deepshikajanarthanan <43632034+Deepshikajanarthanan@users.noreply.github.com>
2022-05-22 20:00:55 +0530

    Add files via upload

---
+++ TheNextStep/db_connection.py
@@ -0,0 +1,41 @@
+import pymongo
+
+def get_data_from_db(Username,Password):
+    print("In DB Connection")
+    DEFAULT_CONNECTION_URL = "mongodb+srv://admin:admin@thenextstep.ynldq.mongodb.net/test"
+    # Establish a connection with mongoDB
+    client = pymongo.MongoClient(DEFAULT_CONNECTION_URL)
+    my_db = client["TheNextStep"]
+    my_collection = my_db["user"]
+    query = {"Username":Username,"Password":Password}
+    data = my_collection.find_one(query)
+    if data == None:
+        return("Invalid Username or Password")
+    else:
+        return("Successfully Logged In")
+
+def signup_connection(fullName,email,password,confirmPassword):
+    print("In DB Connection")
+    DEFAULT_CONNECTION_URL = "mongodb+srv://admin:admin@thenextstep.ynldq.mongodb.net/test"
+    # Establish a connection with mongoDB
+    client = pymongo.MongoClient(DEFAULT_CONNECTION_URL)
+    my_db = client["TheNextStep"]
+    my_collection = my_db["user"]
+    query = {'email':email}
+    data = my_collection.find_one(query)
+    if data == None:
+        my_collection.insert_one({'fullName':fullName,'email':email,'password':password,'confirmPassword':confirmPassword})
+        return ("Successfully Signed Up")
+    else:
+        return ("Email Id already registered")
+
+def get_recommendation(streamName):
+    print("In DB Connection")
+    DEFAULT_CONNECTION_URL = "mongodb+srv://admin:admin@thenextstep.ynldq.mongodb.net/test"
+    # Establish a connection with mongoDB
+    client = pymongo.MongoClient(DEFAULT_CONNECTION_URL)
+    my_db = client["TheNextStep"]
+    my_collection = my_db["recommendation"]
+    query = {'streamName': streamName}
+    data = my_collection.find_one(query)
+    return data
---
+++ TheNextStep/main.py
@@ -0,0 +1,40 @@
+from db_connection import get_data_from_db,signup_connection,get_recommendation
+import flask
+from flask_cors import CORS
+import json
+from flask import request
+
+app = flask.Flask(__name__)
+CORS(app)
+
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    if request.method == 'POST' and 'userName' in request.form and 'password' in request.form:
+        username = request.form['userName']
+        password = request.form['password']
+        results = get_data_from_db(username, password)
+        print(results)
+        return results
+
+@app.route('/signup', methods =['GET', 'POST'])
+def signup():
+    fullName = request.form["fullName"]
+    email = request.form["email"]
+    password = request.form["password"]
+    confirmPassword = request.form["confirmPassword"]
+    put_data = signup_connection(fullName,email,password,confirmPassword)
+    print(put_data)
+    return put_data
+
+@app.route('/recommendation', methods =['GET', 'POST'])
+def recommendation():
+    streamName = request.form["streamName"]
+    recommendation_list = get_recommendation(streamName)
+    rec = json.dumps(recommendation_list, default=str)
+    return rec
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=54321)
